c19c78696b0ab5c926a3ced1b2bd6c2155f144e9
Joseph Parker <jparker010190@gmail.com>
2018-02-28 01:47:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,11 +13,10 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if(string.toLowerCase() === string)
   {console.log("I can\'t hear you!")}
-  else if(string.toUpperCase() === string){
+  if(string.toUpperCase() === string){
     console.log("YES INDEED!")
   }
-  else if (string === 'I love you, Grandma'){
+  if (string === 'I love you, Grandma'){
     console.log('I love you, too.')
   }
-  else()
 }
