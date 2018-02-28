65a37a440a4ba5b06febe163e492f4c7d1c501fe
Joseph Parker <jparker010190@gmail.com>
2018-02-28 01:41:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function sayHiToGrandma(string){
   if(string.toLowerCase() === string)
   {console.log("I can\'t hear you!")}
   else if(string.toUpperCase() === string){
-    console.log("YES INDEED")
+    console.log("YES INDEED!")
   }
   else if (string === 'I love you, Grandma'){
     console.log('I love you, too.')
