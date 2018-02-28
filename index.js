f1a302ee6d740a749ea9ae483984ff324c07e7ba
Joseph Parker <jparker010190@gmail.com>
2018-02-28 01:37:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
   if(string.toLowerCase() === string)
-  {console.log("I can't hear you!")}
+  {console.log("I can\'t hear you!")}
   else if(string.toUpperCase() === string){
     console.log("YES INDEED")
   }
